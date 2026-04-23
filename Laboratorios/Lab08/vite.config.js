3eb728b5727aa4dfb68a803d809f233812f347dd
Jarley Jerez <jarleym213@gmail.com>
2026-04-23 10:48:26 -0600

    viteconfig

--- Laboratorios/Lab08/vite.config.js
+++ Laboratorios/Lab08/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- // base: '/11-51-Progra-Int/Laboratorios/Lab08/dist/',
+  base: '/11-51-Progra-Int/Laboratorios/Lab08/dist/',
 })
